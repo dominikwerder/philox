1e6198413c5173854661f534aed2623fa15a23cb
Dominik Werder <dominik.werder@gmail.com>
2019-03-01 11:19:10 +0100

    Two test cases succeed, only the random case not

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,41 @@ use std::ops::{Mul, Div};
 type Ph<N, W, R> = Philox<N, W, R, <N as Div<U2>>::Output>;
 
 pub struct Philox<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: ArrayLength<u32>> {
-	ctr: GenericArray<u32, N>,
-	key: GenericArray<u32, KN>,
+	_m0: std::marker::PhantomData<N>,
+	_m1: std::marker::PhantomData<KN>,
 	_m2: std::marker::PhantomData<W>,
 	_m3: std::marker::PhantomData<R>,
 }
 
+struct HiLo(u32, u32);
+
+fn mulhilo(a: u32, b: u32) -> HiLo {
+	let p = a as u64 * b as u64;
+	HiLo((p >> 32) as u32, p as u32)
+}
+
 impl<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: ArrayLength<u32>> Philox<N, W, R, KN> {
-	pub fn next(&mut self, _ctr: GenericArray<u32, N>) -> GenericArray<u32, N> {
-		for i in 0..R::USIZE {
-			if i > 0 {
-				self.update_key();
-			}
-			self.update();
+	pub fn next(&mut self, mut key: GenericArray<u32, KN>, mut ctr: GenericArray<u32, N>) -> GenericArray<u32, N> {
+		for _ in 0..R::USIZE {
+			self.round(&mut key, &mut ctr);
+			self.update_key(&mut key);
 		}
-		self.ctr.clone()
+		ctr
 	}
-	fn update(&mut self) {
+	fn round(&mut self, key: &mut GenericArray<u32, KN>, mut ctr: &mut GenericArray<u32, N>) {
+		let c0 = ctr.clone();
+		// These constants were chosen just because the random numbers look statistically best
+		let HiLo(hi0, lo0) = mulhilo(0xD2511F53, c0[0]);
+		let HiLo(hi1, lo1) = mulhilo(0xCD9E8D57, c0[2]);
+		let c1 = &mut ctr;
+		c1[0] = hi1 ^ c0[1] ^ key[0];
+		c1[1] = lo1;
+		c1[2] = hi0 ^ c0[3] ^ key[1];
+		c1[3] = lo0;
 	}
-	fn update_key(&mut self) {
-		self.key[0].overflowing_add(0x9E3779B9); // golden ratio
-		self.key[1].overflowing_add(0xBB67AE85); // sqrt(3) - 1
+	fn update_key(&mut self, key: &mut GenericArray<u32, KN>) {
+		key[0] = key[0].overflowing_add(0x9E3779B9).0; // golden ratio
+		key[1] = key[1].overflowing_add(0xBB67AE85).0; // sqrt(3) - 1
 	}
 }
 
@@ -34,13 +48,13 @@ impl<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: Unsigned + Ar
 	fn default() -> Self {
 		assert_eq!(W::to_usize(), 32);
 		let x = Self {
-			ctr: Default::default(),
-			key: Default::default(),
+			_m0: Default::default(),
+			_m1: Default::default(),
 			_m2: Default::default(),
 			_m3: Default::default(),
 		};
-		assert_eq!(x.ctr.len(), N::to_usize());
-		assert_eq!(x.key.len(), KN::to_usize());
+		//assert_eq!(x.ctr.len(), N::to_usize());
+		//assert_eq!(x.key.len(), KN::to_usize());
 		x
 	}
 }
@@ -52,6 +66,54 @@ impl<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: Unsigned + Ar
 	assert_eq!(U64::U32, 64);
 }
 
-#[test] fn b() {
-	Ph::<U4, U32, U10>::default();
+#[test] fn arithmetic() {
+	assert_eq!((0xffffffffffffffff_u64 >> 32) as u32, 0xffffffff_u32);
+	assert_eq!(0x12345678cafe1337_u64 as u32, 0xcafe1337_u32);
+}
+
+/*
+*/
+
+fn parse_test_vector(s: &str) -> (GenericArray<u32, U2>, GenericArray<u32, U4>, GenericArray<u32, U4>) {
+	let a: Vec<_> = s.split(" ").map(|x| x.trim()).collect();
+	eprintln!("{:?}", a);
+	type T = u32;
+	fn p(s: &str) -> T {
+		T::from_str_radix(s, 16).unwrap()
+	}
+	(
+		[p(a[2]), p(a[3])].into(),
+		[p(a[4]), p(a[5]), p(a[6]), p(a[7])].into(),
+		[p(a[8]), p(a[9]), p(a[10]), p(a[11])].into(),
+	)
+}
+
+#[test] fn test_parse_test_vector() {
+	let s = "philox4x32 10 243f6a88 85a308d3 13198a2e 03707344 a4093822 299f31d0 d16cfe09 94fdcceb 5001e420 24126ea1";
+	let v = parse_test_vector(s);
+	assert_eq!(v.0.as_slice(), &[0x243f6a88, 0x85a308d3]);
+}
+
+#[test] fn generate_key_zeros() {
+	let vectors = "
+	philox4x32 10 00000000 00000000 00000000 00000000 00000000 00000000 6627e8d5 e169c58d bc57ac4c 9b00dbd8
+	philox4x32 10 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff 408f276d 41c83b0e a20bc7c6 6d5451fd
+	philox4x32 10 243f6a88 85a308d3 13198a2e 03707344 a4093822 299f31d0 d16cfe09 94fdcceb 5001e420 24126ea1
+	".trim().split("\n").map(|x|x.trim()).collect::<Vec<_>>();
+	eprintln!("{:?}", vectors);
+	for v in &vectors[..3] {
+		let v = parse_test_vector(v);
+		let mut ph = Ph::<U4, U32, U10>::default();
+		for x in &v.0 {
+			eprintln!("k {:08x}", x);
+		}
+		for x in &v.1 {
+			eprintln!("c {:08x}", x);
+		}
+		let r = ph.next(v.0, v.1);
+		for x in &r {
+			eprintln!("r {:08x}", x);
+		}
+		assert_eq!(r.as_slice(), v.2.as_slice());
+	}
 }
