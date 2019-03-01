71b4e90b081523ec9d7e170260cc72acc9a2a1c4
Dominik Werder <dominik.werder@gmail.com>
2019-03-01 09:20:32 +0100

    Assert correct array sizes

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,34 @@ use typenum::{Unsigned, U2, U4, U8, U10, U32, U64, Same, UInt};
 use generic_array::{ArrayLength, GenericArray};
 use std::ops::{Mul, Div};
 
-type Ph<N, W, R> = Philox<N, W, R, <N as Div<U8>>::Output>;
+type Ph<N, W, R> = Philox<N, W, R, <N as Div<U2>>::Output>;
 
-pub struct Philox<N: Unsigned, W: Unsigned, R: Unsigned, NA: ArrayLength<u32>> {
-	_key: GenericArray<u32, NA>,
+pub struct Philox<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: ArrayLength<u32>> {
+	ctr: GenericArray<u32, N>,
+	key: GenericArray<u32, KN>,
 	_m1: std::marker::PhantomData<N>,
 	_m2: std::marker::PhantomData<W>,
 	_m3: std::marker::PhantomData<R>,
 }
 
-impl<N: Unsigned, W: Unsigned, R: Unsigned, NA: ArrayLength<u32>> Default for Philox<N, W, R, NA> {
+impl<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: ArrayLength<u32>> Philox<N, W, R, KN> {
+	pub fn next(&mut self) {
+	}
+}
+
+impl<N: Unsigned + ArrayLength<u32>, W: Unsigned, R: Unsigned, KN: Unsigned + ArrayLength<u32>> Default for Philox<N, W, R, KN> {
 	fn default() -> Self {
 		assert_eq!(W::to_usize(), 32);
-		Self {
-			_key: Default::default(),
+		let x = Self {
+			ctr: Default::default(),
+			key: Default::default(),
 			_m1: Default::default(),
 			_m2: Default::default(),
 			_m3: Default::default(),
-		}
+		};
+		assert_eq!(x.ctr.len(), N::to_usize());
+		assert_eq!(x.key.len(), KN::to_usize());
+		x
 	}
 }
 
