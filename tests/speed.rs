172905e947a5be287d50836b43c7b092d90182a2
Dominik Werder <dominik.werder@gmail.com>
2019-04-01 10:32:48 +0200

    Refactor speed test to measure predefined interval

--- tests/speed.rs
+++ tests/speed.rs
@@ -23,20 +23,25 @@ impl Counter {
   }
 }
 
-fn speed() {
-  let t1 = std::time::Instant::now();
+#[test] fn speed() {
   let mut ph = Philox4x32_10::default();
   let key = GenericArray::from_slice(&[2, 6]);
   let mut ctr = Counter(GenericArray::default());
-  let nn = 10000000;
-  for _ in 0..nn {
+  let now = std::time::Instant::now;
+  let t1 = now();
+  loop {
     ph.next(key.clone(), ctr.0.clone());
     ctr.inc();
+    if ctr.0[0] & 0x7fff == 0 {
+      if now() - t1 > std::time::Duration::from_millis(1500) {
+        break;
+      }
+    }
   }
-  let t2 = std::time::Instant::now();
-  let dt = t2 - t1;
+  let dt = now() - t1;
   let secs = dt.as_secs() as f32 + 1e-3 * dt.subsec_millis() as f32;
-  println!("Duration: {:?}  MB/s: {:.0}", secs, (nn as f32 * 16.) / secs / 1024.0 / 1024.0);
+  let nn = ((ctr.0[1] as u64) << 32) + ctr.0[0] as u64;
+  eprintln!("Duration: {:?}  MB/s: {:.0}", secs, (nn as f32 * 16.) / secs / 1024.0 / 1024.0);
 }
 
 fn main() {
